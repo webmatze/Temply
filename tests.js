b7905c54852605e30841ea9cc6a9f00e722deb60
Mathias Karstädt <mathias.karstaedt@gmail.com>
2013-02-19 16:42:40 +0100

    added tests

--- tests.js
+++ tests.js
@@ -1,3 +1,15 @@
+module( "Replace and fill template tags" );
+
+test("should fill tp-model tags with data", function() {
+  var fixture = document.getElementById( "qunit-fixture" );
+  var span = document.createElement("span");
+  span.setAttribute("tp-model", "name");
+  fixture.appendChild(span);
+  var temply = new Temply({name: "Test"});
+  temply.run();
+  equal(document.getElementById("qunit-fixture").innerHTML, "<span tp-model=\"name\">Test</span>", "tp-model filled with data");
+});
+
 test("should replace {{name}} template tags", function() {
   var fixture = document.getElementById( "qunit-fixture" );
   fixture.innerHTML = "{{name}}";
