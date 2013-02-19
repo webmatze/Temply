d410decb4d5e70fcc6a46a2dbb7937fb281d67d2
Mathias Karstädt <mathias.karstaedt@gmail.com>
2013-02-19 16:57:12 +0100

    added test for tp-repeat tags

--- tests.js
+++ tests.js
@@ -1,19 +1,22 @@
 module( "Replace and fill template tags" );
 
 test("should fill tp-model tags with data", function() {
-  var fixture = document.getElementById( "qunit-fixture" );
-  var span = document.createElement("span");
-  span.setAttribute("tp-model", "name");
-  fixture.appendChild(span);
+  $("#qunit-fixture").append($("<span tp-model=\"name\"></span>"));
   var temply = new Temply({name: "Test"});
   temply.run();
-  equal(document.getElementById("qunit-fixture").innerHTML, "<span tp-model=\"name\">Test</span>", "tp-model filled with data");
+  equal($("#qunit-fixture").html(), "<span tp-model=\"name\">Test</span>", "tp-model filled with data");
 });
 
 test("should replace {{name}} template tags", function() {
-  var fixture = document.getElementById( "qunit-fixture" );
-  fixture.innerHTML = "{{name}}";
+  $("#qunit-fixture").html("{{name}}");
   var temply = new Temply({name: "World"});
   temply.run();
-  equal(document.getElementById("qunit-fixture").innerHTML, "<span tp-model=\"name\">World</span>", "tag replaced");
+  equal($("#qunit-fixture").html(), "<span tp-model=\"name\">World</span>", "tag replaced");
+});
+
+test("should repeat tp-repeat tags", function() {
+  $("#qunit-fixture").append($("<div tp-repeat=\"entries\" tp-model=\"entry\">{{entry.name}}</div>"));
+  var temply = new Temply({entries: [{name:"Hello"},{name:"World"}]});
+  temply.run();
+  equal($("#qunit-fixture").html(), "<div><span tp-model=\"entry.name\">Hello</span></div><div><span tp-model=\"entry.name\">World</span></div>", "tag repeated");
 });
